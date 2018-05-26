c8655fac26e396496e7e4ac184370fc53145ce1c
Mike Calvanese <mfcalvanese@gmail.com>
2018-05-26 19:40:52 -0300

    white card with bonded curve

--- ethereum/contracts/WhiteCard.sol
+++ ethereum/contracts/WhiteCard.sol
@@ -1,20 +1,293 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
-contract WhiteCardFactory {
-    address[] public whiteCards;
-    
-    function addWhiteCard(string _ipfsHash) public {
-        address _whiteCard = new WhiteCard(_ipfsHash, msg.sender);
-        whiteCards.push(_whiteCard);
+// Abstract contract for the full ERC 20 Token standard
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+
+contract EIP20Interface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    // solhint-disable-next-line no-simple-event-func-name
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+/*
+Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+.*/
+
+contract EIP20 is EIP20Interface {
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg SBX
+
+    function EIP20(
+        uint256 _initialAmount,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol
+    ) public {
+        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
+        totalSupply = _initialAmount;                        // Update total supply
+        name = _tokenName;                                   // Set the name for display purposes
+        decimals = _decimalUnits;                            // Amount of decimals for display purposes
+        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
 }
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/// @title  EthBondingCurvedToken - A bonding curve
+///         implementation that is backed by ether.
+contract EthBondingCurvedToken is EIP20 {
+
+    event Minted(uint256 amount, uint256 totalCost);
+    event Burned(uint256 amount, uint256 reward);
+
+    using SafeMath for uint256;
+
+    uint256 public poolBalance;
+
+    /// @dev constructor    Initializes the bonding curve
+    /// @param name         The name of the token
+    /// @param decimals     The number of decimals to use
+    /// @param symbol       The symbol of the token
+    constructor(
+        string name,
+        uint8 decimals,
+        string symbol
+    ) EIP20(0, name, decimals, symbol) public {}
+
+    /// @dev                Get the price in ether to mint tokens
+    /// @param numTokens    The number of tokens to calculate price for
+    function priceToMint(uint256 numTokens) public returns(uint256);
+
+    /// @dev                Get the reward in ether to burn tokens
+    /// @param numTokens    The number of tokens to calculate reward for
+    function rewardForBurn(uint256 numTokens) public returns(uint256);
+
+    /// @dev                Mint new tokens with ether
+    /// @param numTokens    The number of tokens you want to mint
+    function mint(uint256 numTokens) public payable {
+        uint256 priceForTokens = priceToMint(numTokens);
+        require(msg.value >= priceForTokens);
+
+        totalSupply = totalSupply.add(numTokens);
+        balances[msg.sender] = balances[msg.sender].add(numTokens);
+        poolBalance = poolBalance.add(priceForTokens);
+        if (msg.value > priceForTokens) {
+            msg.sender.transfer(msg.value - priceForTokens);
+        }
+
+        emit Minted(numTokens, priceForTokens);
+    }
+
+    /// @dev                Burn tokens to receive ether
+    /// @param numTokens    The number of tokens that you want to burn
+    function burn(uint256 numTokens) public {
+        require(balances[msg.sender] >= numTokens);
+
+        uint256 ethToReturn = rewardForBurn(numTokens);
+        totalSupply = totalSupply.sub(numTokens);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        poolBalance = poolBalance.sub(ethToReturn);
+        msg.sender.transfer(ethToReturn);
+
+        emit Burned(numTokens, ethToReturn);
+    }
+}
+
+/// @title  EthPolynomialCurvedToken - A polynomial bonding curve
+///         implementation that is backed by ether.
+contract EthPolynomialCurvedToken is EthBondingCurvedToken {
+
+    uint256 constant private PRECISION = 10000000000;
+
+    uint8 public exponent;
+
+    /// @dev constructor        Initializes the bonding curve
+    /// @param name             The name of the token
+    /// @param decimals         The number of decimals to use
+    /// @param symbol           The symbol of the token
+    /// @param _exponent        The exponent of the curve
+    constructor(
+        string name,
+        uint8 decimals,
+        string symbol,
+        uint8 _exponent
+    ) EthBondingCurvedToken(name, decimals, symbol) public {
+        exponent = _exponent;
+    }
+
+    /// @dev        Calculate the integral from 0 to t
+    /// @param t    The number to integrate to
+    function curveIntegral(uint256 t) internal returns (uint256) {
+        uint256 nexp = exponent + 1;
+        // Calculate integral of t^exponent
+        return PRECISION.div(nexp).mul(t ** nexp).div(PRECISION);
+    }
+
+    function priceToMint(uint256 numTokens) public returns(uint256) {
+        return getMintingPrice(numTokens);
+    }
+
+    function rewardForBurn(uint256 numTokens) public returns(uint256) {
+        return getBurningReward(numTokens);
+    }
+    
+    function getMintingPrice(uint256 numTokens) public view returns(uint256) {
+        return curveIntegral(totalSupply.add(numTokens)).sub(poolBalance);
+    }
+
+    function getBurningReward(uint256 numTokens) public view returns(uint256) {
+        return poolBalance.sub(curveIntegral(totalSupply.sub(numTokens)));
+    }
+}
+
+contract WhiteCardFactory {
+    event _WhiteCardCreated(WhiteCard card);
+    
+    function addWhiteCard(string _ipfsHash) public {
+        WhiteCard _whiteCard = new WhiteCard(_ipfsHash, msg.sender);
+        
+        emit _WhiteCardCreated(_whiteCard);
+    }
+}
+
 contract WhiteCard {
     
     string public ipfsHash;
     address public creator;
+    EthPolynomialCurvedToken public bondingCurve;
     
-    constructor(string _ipfsHash, address _creator) public {
+    constructor (string _ipfsHash, address _creator) public {
         ipfsHash = _ipfsHash;
         creator = _creator;
+        bondingCurve = new EthPolynomialCurvedToken(_ipfsHash, 18, "", 1);
     }
 }
